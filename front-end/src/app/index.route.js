bedad5e946e5f773b0df0b6e153edf6fe46bfb67
Ed Orsini <edorsini@mit.edu>
2017-02-09 13:51:32 -0500

    Created new route and place holder HTML for 'profile picture and upload' feature

--- front-end/src/app/index.route.js
+++ front-end/src/app/index.route.js
@@ -1,18 +1,24 @@
-export function routerConfig ($stateProvider, $urlRouterProvider) {
-  'ngInject';
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'main'
-    })
-    .state('auth', {
-      url: '/auth',
-      templateUrl: 'app/auth/auth.html',
-      controller: 'AuthController',
-      controllerAs: 'auth'
-    });
+export function routerConfig($stateProvider, $urlRouterProvider) {
+    'ngInject';
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/main/main.html',
+            controller: 'MainController',
+            controllerAs: 'main'
+        })
+        .state('auth', {
+            url: '/auth',
+            templateUrl: 'app/auth/auth.html',
+            controller: 'AuthController',
+            controllerAs: 'auth'
+        })
+        .state('picture', {
+            url: '/picture',
+            templateUrl: 'app/picture/picture.html',
+            controller: 'PictureController',
+            controllerAs: 'picture'
+        });
 
-  $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 }
---
+++ front-end/src/app/picture/picture.controller.js
@@ -0,0 +1,21 @@
+export class PictureController {
+    constructor($http) {
+        'ngInject';
+
+        this.$http = $http;
+        this.getMessages();
+    }
+
+    getMessages() {
+        var vm = this;
+        this.$http.get('http://localhost:5000/api/message').then(function(result) {
+            vm.messages = result.data;
+        });
+    }
+
+    postMessage() {
+        this.$http.post('http://localhost:5000/api/message', { msg: this.message });
+    }
+
+
+}
